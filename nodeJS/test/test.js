5b407df3ffa9533dabcb42b390aad328093b54e9
taher mabrouk <152318856+Tahermabrouk1@users.noreply.github.com>
2025-05-13 23:38:08 +0100

    Update test.js

--- nodeJS/test/test.js
+++ nodeJS/test/test.js
@@ -57,174 +57,76 @@ describe("Employee Route Unit Tests", () => {
           done();
         });
     });
+  });
 
-    //   it("should return 500 on database error", (done) => {
-    //     sinon.stub(Employee, "find").rejects(new Error("Database error"));
+  describe("GET /employees/:id", () => {
+    it("should return an employee by ID", (done) => {
+      const mockEmployee = { _id: "1", name: "John Doe", role: "doctor" };
+      sinon.stub(Employee, "findById").withArgs("1").resolves(mockEmployee);
 
-    //     chai
-    //       .request(app)
-    //       .get("/employees")
-    //       .end((err, res) => {
-    //         if (err) return done(err);
-    //         res.should.have.status(500);
-    //         res.body.should.have.property("message").equal("Database error");
-    //         done();
-    //       });
-    //   });
-    // });
-
-    describe("GET /employees/:id", () => {
-      it("should return an employee by ID", (done) => {
-        const mockEmployee = { _id: "1", name: "John Doe", role: "doctor" };
-        sinon.stub(Employee, "findById").withArgs("1").resolves(mockEmployee);
-
-        chai
-          .request(app)
-          .get("/employees/1")
-          .end((err, res) => {
-            if (err) return done(err);
-            res.should.have.status(200);
-            res.body.should.deep.equal(mockEmployee);
-            done();
-          });
-      });
-
-      // it("should return 404 if employee not found", (done) => {
-      //   sinon.stub(Employee, "findById").withArgs("1").resolves(null);
-
-      //   chai
-      //     .request(app)
-      //     .get("/employees/1")
-      //     .end((err, res) => {
-      //       if (err) return done(err);
-      //       res.should.have.status(404);
-      //       res.body.should.have.property("message").equal("Employé non trouvé");
-      //       done();
-      //     });
-      // });
-
-      //   it("should return 500 on database error", (done) => {
-      //     sinon.stub(Employee, "findById").rejects(new Error("Database error"));
-
-      //     chai
-      //       .request(app)
-      //       .get("/employees/1")
-      //       .end((err, res) => {
-      //         if (err) return done(err);
-      //         res.should.have.status(500);
-      //         res.body.should.have.property("message").equal("Database error");
-      //         done();
-      //       });
-      //   });
-      // });
-
-      describe("DELETE /employees/:id", () => {
-        it("should delete an employee by ID", (done) => {
-          const mockEmployee = { _id: "1", name: "John Doe", role: "doctor" };
-          sinon
-            .stub(Employee, "findByIdAndDelete")
-            .withArgs("1")
-            .resolves(mockEmployee);
-
-          chai
-            .request(app)
-            .delete("/employees/1")
-            .end((err, res) => {
-              if (err) return done(err);
-              res.should.have.status(204);
-              res.body.should.be.empty;
-              done();
-            });
+      chai
+        .request(app)
+        .get("/employees/1")
+        .end((err, res) => {
+          if (err) return done(err);
+          res.should.have.status(200);
+          res.body.should.deep.equal(mockEmployee);
+          done();
         });
+    });
+  });
 
-        // it('should return 404 if employee not found', (done) => {
-        //   sinon.stub(Employee, 'findByIdAndDelete').withArgs('1').resolves(null);
+  describe("DELETE /employees/:id", () => {
+    it("should delete an employee by ID", (done) => {
+      const mockEmployee = { _id: "1", name: "John Doe", role: "doctor" };
+      sinon
+        .stub(Employee, "findByIdAndDelete")
+        .withArgs("1")
+        .resolves(mockEmployee);
 
-        //   chai.request(app)
-        //     .delete('/employees/1')
-        //     .end((err, res) => {
-        //       if (err) return done(err);
-        //       res.should.have.status(404);
-        //       res.body.should.have.property('message').equal('Employé non trouvé');
-        //       done();
-        //     });
-        // });
+      chai
+        .request(app)
+        .delete("/employees/1")
+        .end((err, res) => {
+          if (err) return done(err);
+          res.should.have.status(204);
+          res.body.should.be.empty;
+          done();
+        });
+    });
+  });
 
-        //   it('should return 500 on database error', (done) => {
-        //     sinon.stub(Employee, 'findByIdAndDelete').rejects(new Error('Delete error'));
+  describe("PUT /employees/:id", () => {
+    it("should update an employee by ID", (done) => {
+      const existingEmployee = {
+        _id: "1",
+        name: "John Doe",
+        role: "doctor",
+      };
+      const updatedData = { name: "John Updated", role: "nurse" };
+      const updatedEmployee = { _id: "1", ...updatedData };
+      sinon
+        .stub(Employee, "findById")
+        .withArgs("1")
+        .resolves(existingEmployee);
+      sinon
+        .stub(Employee, "findByIdAndUpdate")
+        .withArgs("1", { $set: updatedData }, { new: true })
+        .resolves(updatedEmployee);
 
-        //     chai.request(app)
-        //       .delete('/employees/1')
-        //       .end((err, res) => {
-        //         if (err) return done(err);
-        //         res.should.have.status(500);
-        //         res.body.should.have.property('message').equal('Erreur serveur lors de la suppression de l\'employé');
-        //         done();
-        //       });
-        //   });
-        // });
-
-        describe("PUT /employees/:id", () => {
-          it("should update an employee by ID", (done) => {
-            const existingEmployee = {
-              _id: "1",
-              name: "John Doe",
-              role: "doctor",
-            };
-            const updatedData = { name: "John Updated", role: "nurse" };
-            const updatedEmployee = { _id: "1", ...updatedData };
-            sinon
-              .stub(Employee, "findById")
-              .withArgs("1")
-              .resolves(existingEmployee);
-            sinon
-              .stub(Employee, "findByIdAndUpdate")
-              .withArgs("1", { $set: updatedData }, { new: true })
-              .resolves(updatedEmployee);
-
-            chai
-              .request(app)
-              .put("/employees/1")
-              .send(updatedData)
-              .end((err, res) => {
-                if (err) return done(err);
-                res.should.have.status(200);
-                res.body.should.deep.equal(updatedEmployee);
-                done();
-              });
-          });
-
-          // it('should return 404 if employee not found', (done) => {
-          //   sinon.stub(Employee, 'findById').withArgs('1').resolves(null);
-
-          //   chai.request(app)
-          //     .put('/employees/1')
-          //     .send({ name: 'John Updated' })
-          //     .end((err, res) => {
-          //       if (err) return done(err);
-          //       res.should.have.status(404);
-          //       res.body.should.have.property('message').equal('Employee not found');
-          //       done();
-          //     });
-          // });
-
-          //   it('should return 500 on database error', (done) => {
-          //     const existingEmployee = { _id: '1', name: 'John Doe', role: 'doctor' };
-          //     sinon.stub(Employee, 'findById').resolves(existingEmployee);
-          //     sinon.stub(Employee, 'findByIdAndUpdate').rejects(new Error('Update error'));
-
-          //     chai.request(app)
-          //       .put('/employees/1')
-          //       .send({ name: 'John Updated' })
-          //       .end((err, res) => {
-          //         if (err) return done(err);
-          //         res.should.have.status(500);
-          //         res.body.should.have.property('message').equal('Error updating employee data');
-          //         res.body.should.have.property('error').equal('Update error');
-          //         done();
-          //       });
-          //   });
-          // });
+      chai
+        .request(app)
+        .put("/employees/1")
+        .send(updatedData)
+        .end((err, res) => {
+          if (err) return done(err);
+          res.should.have.status(200);
+          res.body.should.deep.equal(updatedEmployee);
+          done();
+        });
+    });
+  });
+});
 
 /*
 describe("POST /addEmployee", () => {
@@ -263,27 +165,3 @@ describe("POST /addEmployee", () => {
   });
 });
 */
-
-            // it('should return 400 for validation errors', (done) => {
-            //   const invalidEmployee = { role: 'technician' }; // Missing required fields
-            //   sinon.stub(Employee.prototype, 'save').rejects({
-            //     name: 'ValidationError',
-            //     message: 'Path `email` is required.',
-            //   });
-
-            //   chai.request(app)
-            //     .post('/addEmployee')
-            //     .send(invalidEmployee)
-            //     .end((err, res) => {
-            //       if (err) return done(err);
-            //       res.should.have.status(400);
-            //       res.body.should.have.property('message').equal('Path `email` is required.');
-            //       done();
-            //     });
-            // });
-          });
-        });
-      });
-    });
-  });
-});
